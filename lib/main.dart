61cb65fc86f4fccd4f8c22998bf9bebbccc0189f
久保一誠 <issei.0904@icloud.com>
2020-08-01 20:20:46 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Adultify'),
+        title: Text('Adultify2'),
       ),
       body: Container(
         width: double.infinity,
